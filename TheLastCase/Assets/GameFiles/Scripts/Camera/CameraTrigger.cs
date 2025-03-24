fb668704da20a8202e2dfcaa8e7d56de19d66a18
Alec Robinson <s4203312@glos.ac.uk>
2025-03-24 16:14:37 +0000

    .

--- TheLastCase/Assets/GameFiles/Scripts/Camera/CameraTrigger.cs
+++ TheLastCase/Assets/GameFiles/Scripts/Camera/CameraTrigger.cs
@@ -33,17 +33,12 @@ public class CameraTrigger : MonoBehaviour
         }
         else               //Follow if corridoors
         {
-            //Quaternion startRotation = gameCam.transform.rotation;
-            //Quaternion endRotation = new Quaternion(86.667f, -90.398f, 0.0151f , 1);
-
             if (playerController.isGhostActive)
             {
-                //gameCam.transform.rotation = endRotation;
                 gameCam.Follow = GameObject.Find("Ghost").transform;
             }
             else
             {
-                //gameCam.transform.rotation = endRotation;
                 gameCam.Follow = GameObject.Find("Player").transform;
             }
         }
