d7fcf322b5984a50b7c347203fc1a4ebd0bc2a3b
Alec Robinson <s4203312@glos.ac.uk>
2025-04-20 18:59:06 +0100

    inspection interaction

--- TheLastCase/Assets/GameFiles/Scripts/Player/Interaction/InspectObject.cs
+++ TheLastCase/Assets/GameFiles/Scripts/Player/Interaction/InspectObject.cs
@@ -1,6 +1,7 @@
 using Cinemachine;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.AI.Navigation;
 using UnityEngine;
 
 public class InspectObject : MonoBehaviour
@@ -12,9 +13,12 @@ public class InspectObject : MonoBehaviour
     private float dRotationY;
     private float rotationSpeed = 4;
 
+    private CinemachineVirtualCamera virtualCam;
+
     private void Start()
     {
         inventoryLoadScipt = GameObject.Find("InventoryManager").GetComponent<UIInventoryLoad>();
+        virtualCam = GameObject.Find("VirtualCamera").GetComponent<CinemachineVirtualCamera>();
     }
 
     private void Update()
@@ -31,6 +35,26 @@ public class InspectObject : MonoBehaviour
                 inspectingObject.transform.rotation = Quaternion.AngleAxis(dRotationX * rotationSpeed, transform.up) *  //Rotates on X axis
                     Quaternion.AngleAxis(dRotationY * rotationSpeed, transform.right) *                                 //Rotates on Y axis
                     inspectingObject.transform.rotation;                                                                //Multiplying the original rotation
+            }
+
+            //Interacts with object if possible
+            if (Input.GetMouseButtonDown(0) && inspectingObject != null)
+            {
+                var gameCam = GameObject.Find("Main Camera").GetComponent<CinemachineBrain>();
+
+                gameCam.OutputCamera.transform.position = virtualCam.transform.position;
+                gameCam.OutputCamera.transform.rotation = virtualCam.transform.rotation;
+
+                Ray ray = gameCam.OutputCamera.ScreenPointToRay(Input.mousePosition);
+                RaycastHit hit;
+
+                if (Physics.Raycast(ray, out hit, 100))
+                {
+                    if (hit.transform.tag == "InspectInteractable")
+                    {
+                        Debug.Log("Play animation here");
+                    }
+                }
             }
         }
     }
